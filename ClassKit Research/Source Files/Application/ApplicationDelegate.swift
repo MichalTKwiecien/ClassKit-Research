d2418c12290020fc5352465e0f8888fb2af034a2
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-06-22 08:29:14 +0200

    Initit from template

---
+++ ClassKit Research/Source Files/Application/ApplicationDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ClassKit Research
+//
+
+
+import UIKit
+
+@UIApplicationMain
+private final class ApplicationDelegate: UIResponder, UIApplicationDelegate {
+
+    fileprivate lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# ClassKit Research
+
+## Tools & Services
+
+### Tools:
+* Xcode 9.4
+
+## Configuration
+
+### Instalation
+
+1. Clone repository:
+2. Open `DutieSplit.xcodeproj` file and build the project.
