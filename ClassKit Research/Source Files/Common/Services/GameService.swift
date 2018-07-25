ab9a8e96f10669939a62fe37e81c90773ac9ff36
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-07-25 20:33:54 +0200

    Removed not necessary step

--- ClassKit Research/Source Files/Common/Services/GameService.swift
+++ ClassKit Research/Source Files/Common/Services/GameService.swift
@@ -10,8 +10,6 @@ final class GameService {
     
     private(set) var modules = [Module]()
     
-    // TODO 4: Add optional value for storing currently used CLSActivity.
-    
     init() {
         modules.append(ExerciseModuleGeneratorService.generateEasyMathModule1(for: self))
         modules.append(ExerciseModuleGeneratorService.generateEasyMathModule2(for: self))
@@ -19,15 +17,15 @@ final class GameService {
     }
     
     func didStart(module: Module) {
-        // TODO 5: Start new activity and save reference to it.
+        // TODO 4: Start new activity and save reference to it.
     }
     
     func didUpdatedAnswerInside(module: Module) {
-        // TODO 6: Add additional ClassKit item with answered state to your activity.
+        // TODO 5: Add additional ClassKit item with answered state to your activity.
     }
     
     func didFinish(module: Module) {
-        // TODO 7: Publish score to ClassKit and finish the activity.
+        // TODO 6: Publish score to ClassKit and finish the activity.
     }
     
     private func setupClassKitContexts() {
